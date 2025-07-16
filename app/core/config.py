3acf7e956135ce6d0da7fce55f8cd8fa60dcda20
Muhammad Alif Saddid <muhammad.saddid@gopay.co.id>
2025-07-16 10:52:16 +0700

    chore: Fix config

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     REPLICATE_API_TOKEN: str
     GOOGLE_APPLICATION_CREDENTIALS: str
+    GOOGLE_CREDENTIALS_JSON: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
